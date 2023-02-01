aa34620642c64ceb5fd625c83b22fc7f50e823ff
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-01-31 17:42:54 -0700

    fix(BigInt): Add support for LocalView
    * fix: support BigInt64Array and BigUint64Array
    * Update trame_vtk/modules/vtk/addon_serializer.py
    * Add PyVista Int64 validation example
    * docs(example): Improve int64 validation example
    * fix(BigInt): Update vue-vtk-js
    
    ---------
    
    Co-authored-by: Bane Sullivan <bane.sullivan@kitware.com>

---
+++ examples/validation/PyVistaInt64.py
@@ -0,0 +1,56 @@
+"""Validate Int64 usage with VTK.js."""
+
+import pyvista as pv
+import numpy as np
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vuetify
+from trame.widgets.vtk import VtkLocalView, VtkRemoteView
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+state.trame__title = "Int64 Validation"
+
+# -----------------------------------------------------------------------------
+
+mesh = pv.Sphere()
+mesh["data"] = np.arange(mesh.n_cells, dtype=np.int64)
+
+plotter = pv.Plotter(off_screen=True)
+actor = plotter.add_mesh(mesh, scalars="data")
+plotter.reset_camera()
+
+
+# -----------------------------------------------------------------------------
+# GUI
+# -----------------------------------------------------------------------------
+
+with SinglePageLayout(server) as layout:
+    layout.icon.click = ctrl.view_reset_camera
+    layout.title.set_text(state.trame__title)
+
+    with layout.toolbar:
+        vuetify.VSpacer()
+
+    with layout.content:
+        with vuetify.VContainer(
+            fluid=True,
+            classes="pa-0 fill-height",
+        ):
+            with vuetify.VCol(classes="fill-height"):
+                view = VtkLocalView(plotter.ren_win)
+                ctrl.view_update = view.update
+                ctrl.view_reset_camera = view.reset_camera
+            with vuetify.VCol(classes="fill-height"):
+                VtkRemoteView(plotter.ren_win)
+
+    # hide footer
+    layout.footer.hide()
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    server.start()
