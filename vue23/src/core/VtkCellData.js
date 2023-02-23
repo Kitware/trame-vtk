9cd279a3dee3e88aeb0891f370eb65a6a1e3aa1c
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-02-23 13:46:36 -0700

    fix(web): Migrate JS into vue-vtk-js>=3

--- vue23/src/core/VtkCellData.js
+++
@@ -1,9 +0,0 @@
-import { inject, provide } from "vue";
-
-export default {
-  setup() {
-    const dataset = inject("dataset");
-    provide("fields", dataset?.getCellData());
-  },
-  template: '<div class="vtk-cell-data"><slot></slot></div>',
-};
