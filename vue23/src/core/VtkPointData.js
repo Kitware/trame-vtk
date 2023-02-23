c46dc61029ee087b1bbb8a3c4b5c3d04c3f23c87
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-02-23 13:46:36 -0700

    fix(web): Migrate JS into vue-vtk-js>=3

--- vue23/src/core/VtkPointData.js
+++
@@ -1,9 +0,0 @@
-import { inject, provide } from "vue";
-
-export default {
-  setup() {
-    const dataset = inject("dataset");
-    provide("fields", dataset?.getPointData());
-  },
-  template: '<div class="vtk-point-data"><slot></slot></div>',
-};
--- vue23/src/main.js
+++
@@ -1,9 +0,0 @@
-// Ensure vtk.js classes available for Algorithm and Reader
-import "./AvailableClasses";
-import components from "./components";
-
-export function install(Vue) {
-  Object.keys(components).forEach((name) => {
-    Vue.component(name, components[name]);
-  });
-}
