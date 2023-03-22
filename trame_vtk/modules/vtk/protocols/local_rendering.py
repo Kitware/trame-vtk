1bec1497279b1c95337bf9245ab7bfcccd730810
Patrick Avery <patrick.avery@kitware.com>
2023-03-22 10:40:47 -0500

    refactor(render_window_serializer): rename to serializers
    This name fits better since it contains all kinds of serializers.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- trame_vtk/modules/vtk/protocols/local_rendering.py
+++ trame_vtk/modules/vtk/protocols/local_rendering.py
@@ -1,6 +1,6 @@
 from wslink import register as exportRpc
 
-from .render_window_serializer import (
+from .serializers import (
     serializeInstance,
     SynchronizationContext,
     getReferenceId,
