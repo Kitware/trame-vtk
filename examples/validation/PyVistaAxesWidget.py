eaeeeb3c64e7d5a492bfb2764e2d49b7096c8ac5
Bane Sullivan <bane.sullivan@kitware.com>
2023-03-29 15:25:38 -0600

    add pyvista axes widget examples

---
+++ examples/validation/PyVistaAxesWidget.py
@@ -0,0 +1,58 @@
+"""Validate axes actor serialization."""
+
+import pyvista as pv
+import numpy as np
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vuetify
+from trame.widgets.vtk import VtkLocalView, VtkRemoteView
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+state.trame__title = "Axes Widget Validation"
+
+# -----------------------------------------------------------------------------
+
+mesh = pv.Cone()
+
+plotter = pv.Plotter(off_screen=True)
+actor = plotter.add_mesh(mesh)
+plotter.reset_camera()
+
+axes_actor = plotter.add_axes()
+axes_widget = plotter.renderer.axes_widget
+
+
+# -----------------------------------------------------------------------------
+# GUI
+# -----------------------------------------------------------------------------
+
+with SinglePageLayout(server) as layout:
+    layout.icon.click = ctrl.view_reset_camera
+    layout.title.set_text(state.trame__title)
+
+    with layout.toolbar:
+        vuetify.VSpacer()
+
+    with layout.content:
+        with vuetify.VContainer(
+            fluid=True,
+            classes="pa-0 fill-height",
+        ):
+            with vuetify.VCol(classes="fill-height"):
+                view = VtkLocalView(plotter.ren_win)
+                ctrl.view_update = view.update
+                ctrl.view_reset_camera = view.reset_camera
+            with vuetify.VCol(classes="fill-height"):
+                VtkRemoteView(plotter.ren_win)
+
+    # hide footer
+    layout.footer.hide()
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    server.start()
