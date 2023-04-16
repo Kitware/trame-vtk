914669478cb2ba088131d8da81ddc287003afb23
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-04-16 11:24:57 -0600

    docs(pv): Add paraview validation example

---
+++ examples/validation/PvConeRemoteLocal.py
@@ -0,0 +1,88 @@
+# pvpython ./PvConeRemoteLocal.py --venv /path/to/venv
+# pvpython ./PvConeRemote.py --venv /path/to/venv
+import paraview.web.venv
+
+from trame.app import get_server
+from trame.widgets import vuetify, paraview, html
+from trame.ui.vuetify import SinglePageLayout
+
+from paraview import simple
+
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
+
+server = get_server()
+state, ctrl = server.state, server.controller
+
+# -----------------------------------------------------------------------------
+# ParaView code
+# -----------------------------------------------------------------------------
+
+DEFAULT_RESOLUTION = 6
+
+cone = simple.Cone()
+representation = simple.Show(cone)
+view = simple.Render()
+
+
+@state.change("resolution")
+def update_cone(resolution, **kwargs):
+    cone.Resolution = resolution
+    ctrl.view_update()
+
+
+def update_reset_resolution():
+    state.resolution = DEFAULT_RESOLUTION
+
+
+# -----------------------------------------------------------------------------
+# GUI
+# -----------------------------------------------------------------------------
+
+state.trame__title = "ParaView cone"
+
+with SinglePageLayout(server) as layout:
+    layout.icon.click = ctrl.view_reset_camera
+    layout.title.set_text("Cone Application")
+
+    with layout.toolbar:
+        vuetify.VSpacer()
+        vuetify.VSlider(
+            v_model=("resolution", DEFAULT_RESOLUTION),
+            min=3,
+            max=60,
+            step=1,
+            hide_details=True,
+            dense=True,
+            style="max-width: 300px",
+        )
+        html.Span("{{ rendering_mode }}")
+        vuetify.VCheckbox(
+            v_model=("rendering_mode", "local"),
+            true_value="remote",
+            false_value="local",
+            on_icon="mdi-image-area",
+            off_icon="mdi-rotate-3d",
+            hide_details=True,
+            dense=True,
+            classes="my-0 py-0",
+        )
+        vuetify.VDivider(vertical=True, classes="mx-2")
+        with vuetify.VBtn(icon=True, click=update_reset_resolution):
+            vuetify.VIcon("mdi-undo-variant")
+
+    with layout.content:
+        with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
+            html_view = paraview.VtkRemoteLocalView(
+                view, mode=("rendering_mode", "local"), ref="view"
+            )
+            ctrl.view_reset_camera = html_view.reset_camera
+            ctrl.view_update = html_view.update
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    server.start()
