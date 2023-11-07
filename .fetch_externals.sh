6b4e289f3d907d3a850f1905a174e646211e8e7f
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-11-07 09:28:31 -0700

    fix(vtk.js): Bump vtk.js to 29.1.1

--- .fetch_externals.sh
+++ .fetch_externals.sh
@@ -1,3 +1,3 @@
 mkdir -p ./trame_vtk/modules/common/serve
-curl https://unpkg.com/vue-vtk-js@3.1.7 -Lo ./trame_vtk/modules/common/serve/trame-vtk.js
+curl https://unpkg.com/vue-vtk-js@3.1.8 -Lo ./trame_vtk/modules/common/serve/trame-vtk.js
 curl https://kitware.github.io/vtk-js/examples/OfflineLocalView/OfflineLocalView.html -Lo ./trame_vtk/tools/static_viewer.html
