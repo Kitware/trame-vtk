c82ffeca9d6d3bddbd65d630f3c6fc9864352eae
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-05-06 17:27:01 -0400

    fix(js): track hash of vue-vtk-js as well
    Similar to offline_viewer.html we track and report mismatched hash
    of all external dependencies.
    see also https://github.com/Kitware/trame-vtk/pull/70

--- .fetch_externals.sh
+++ .fetch_externals.sh
@@ -4,9 +4,8 @@ set -e
 
 mkdir -p ./trame_vtk/modules/common/serve
 curl https://unpkg.com/vue-vtk-js@3.2.1 -Lo ./trame_vtk/modules/common/serve/trame-vtk.js
-# echo 19e7c20470b952cc7b38f1434180b5ec ./trame_vtk/modules/common/serve/trame-vtk.js | md5sum -c --status && echo OK
 curl https://kitware.github.io/vtk-js/examples/OfflineLocalView/OfflineLocalView.html -Lo ./trame_vtk/tools/static_viewer.html
 
-if ! sha256sum --check .static_viewer.sha256 ; then
-  echo "Hash for static_viewer.html differs, please update .static_viewer.sha256"
+if ! sha256sum --check .externals.sha256 ; then
+  echo "Hash(es) for externals differs, please update .externals.sha256"
 fi
