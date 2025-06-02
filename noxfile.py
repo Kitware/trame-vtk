b227fef764d67a0e4f8c104137c2201b38a1e047
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-06-02 13:09:14 -0600

    tests: update dep and baseline

--- noxfile.py
+++ noxfile.py
@@ -2,8 +2,8 @@ import nox
 
 
 # "3.10", "3.11", "3.12"
-@nox.session(python=["3.9"])
-@nox.parametrize("vtk", ["==9.3.1", "==9.4.1", "==9.4.20250111.dev0"])
+@nox.session(python=["3.10"])
+@nox.parametrize("vtk", ["==9.4.2", "==9.5.20250531.dev0"])
 def tests(session, vtk):
     session.install(".[dev]")
     session.install("trame")
@@ -11,7 +11,7 @@ def tests(session, vtk):
     session.install("trame-vuetify")
     session.install("pytest-asyncio")
     session.install("coverage")
-    session.install("pyvista==0.44.1")
+    session.install("pyvista==0.45.2")
     session.install(f"vtk{vtk}", "--extra-index-url", "https://wheels.vtk.org")
 
     session.run("pytest", "--firefox", "--headless")
