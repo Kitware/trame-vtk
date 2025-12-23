62e97127bbc0fdcb0f9e95cfedff03ec874c9a9f
jonathan.bouyer <jonathan.bouyer@kitware.com>
2025-12-23 12:27:54 +0100

    fix(vue-vtk-js): Bump vue-vtk-js to 3.3.4

--- .fetch_externals.sh
+++ .fetch_externals.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir -p ./src/trame_vtk/modules/common/serve
-curl https://unpkg.com/vue-vtk-js@3.3.3 -Lo ./src/trame_vtk/modules/common/serve/trame-vtk.js
+curl https://unpkg.com/vue-vtk-js@3.3.4 -Lo ./src/trame_vtk/modules/common/serve/trame-vtk.js
 curl https://kitware.github.io/vtk-js/examples/OfflineLocalView/OfflineLocalView.html -Lo ./src/trame_vtk/tools/static_viewer.html
 
 if ! sha256sum --check .externals.sha256 ; then
