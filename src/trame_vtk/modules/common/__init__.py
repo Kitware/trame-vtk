e5b48e46eaddacf83d6468c759b2a40b7edda5ca
Jo-Byr <jonathanbouyer24@gmail.com>
2026-02-04 15:55:13 +0100

    feat: add served directory versioning

--- src/trame_vtk/modules/common/__init__.py
+++ src/trame_vtk/modules/common/__init__.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 
+from trame_vtk import __version__
+
+
 # Compute local path to serve
 serve_path = str(Path(__file__).with_name("serve").resolve())
+serve_directory = f"__trame_vtk_{__version__}"
 
 # Serve directory for JS/CSS files
-serve = {"__trame_vtk": serve_path}
+serve = {serve_directory: serve_path}
 
 # List of JS files to load (usually from the serve path above)
-scripts = ["__trame_vtk/trame-vtk.js"]
+scripts = [f"{serve_directory}/trame-vtk.js"]
 
 # List of Vue plugins to install/load
 vue_use = ["vue_vtk"]
