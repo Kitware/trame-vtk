d4d8d18e6905bac55b924c0c3af761e5a47f3be5
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-03-14 22:15:14 -0600

    fix(static_viewer): fetch latest static viewer

--- .fetch_externals.sh
+++ .fetch_externals.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p ./src/trame_vtk/modules/common/serve
 curl https://unpkg.com/vue-vtk-js@3.3.4 -Lo ./src/trame_vtk/modules/common/serve/trame-vtk.js
-curl https://kitware.github.io/vtk-js/examples/OfflineLocalView/OfflineLocalView.html -Lo ./src/trame_vtk/tools/static_viewer.html
+curl https://kitware.github.io/vtk-js/examples/OfflineLocalView/index.html -Lo ./src/trame_vtk/tools/static_viewer.html
 
 if ! sha256sum --check .externals.sha256 ; then
   echo "Hash(es) for externals differs, please update .externals.sha256"
