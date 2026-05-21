d9de5789604696d51667188a43bc3f8c9cad83d8
Jo-Byr <jonathanbouyer24@gmail.com>
2026-05-21 08:48:42 +0200

    fix(cache): clean cache on object deletion
    Remove cached properties of deleted objects to reduce cache size
    and prevent cache corruption

--- src/trame_vtk/modules/vtk/serializers/serialize.py
+++ src/trame_vtk/modules/vtk/serializers/serialize.py
@@ -1,5 +1,7 @@
 import logging
+import vtk
 
+from .cache import remove_from_cache
 from .registry import class_name, SERIALIZERS
 from .widgets import handle_widget
 
@@ -9,11 +11,17 @@ logger = logging.getLogger(__name__)
 
 # Keep track of which warnings have been printed
 NO_SERIALIZER_FOR_INSTANCE = {}
+DELETE_CALLBACKS = []
 
 
 def serialize(parent, instance, instance_id, context, depth):
     instance_type = class_name(instance)
     serializer = SERIALIZERS[instance_type] if instance_type in SERIALIZERS else None
+    if instance_id not in DELETE_CALLBACKS:
+        instance.AddObserver(
+            vtk.vtkCommand.DeleteEvent, lambda *a, **k: remove(instance_id)
+        )
+        DELETE_CALLBACKS.append(instance_id)
 
     if serializer:
         return serializer(parent, instance, instance_id, context, depth)
@@ -24,6 +32,11 @@ def serialize(parent, instance, instance_id, context, depth):
         NO_SERIALIZER_FOR_INSTANCE[instance_type] = instance_id
 
     return None
+
+
+def remove(instance_id):
+    remove_from_cache(instance_id)
+    DELETE_CALLBACKS.remove(instance_id)
 
 
 def serialize_widget(dict_out, widget):
